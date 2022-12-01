840210456e4e85a2c3bfc8af6a12a2185d022a6d
Oscar <ocisneros1@csu.fullerton.edu>
2022-11-30 17:41:49 -0800

    basic UI

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from typing import List
 import json
-
-class User(BaseModel):
-    username: str
-    email: str
-    password: str
+from pydantic import BaseModel
 
 class Todo(BaseModel):
     id: int
     title: str
     description: str
-    owner: str
     completed: bool
-    edited: bool
 
+# import the mock data as a dict from a json file.
+def fetchData() -> List[Todo]:
+    with open('todos.json') as file:
+        todos: List[Todo] = json.load(file)
+        return todos
+# Instatiate the FastAPI object and set the default templates directory
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="./templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    todo_list: List[Todo] = fetchData()
+    for todo in todo_list:
+        title: str = todo["title"]
+        todo["title"] = title.title()
+    return templates.TemplateResponse("index.html", {"request": request, "todo_list": todo_list})
 
-@app.get("/todos")
-async def get_todos():
-    with open('todos.json') as todos:
-        data = json.load(todos)
-        return data
+@app.get("/todos/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    todo: Todo = [todo for todo in fetchData() if todo["id"] == id][0]
+    title: str = todo["title"]
+    description: str = todo['description']
+    return templates.TemplateResponse("todo.html", {"request": request, "id": id, "title": title.title(), "description": description})
 
-@app.post("/todos/{id}")
-async def create_todo(todo: Todo):
-    return todo
+@app.post("/todos", response_class=RedirectResponse)
+async def add_todo(title: str = Form(), description: str = Form()):
+    todos: List[Todo] = fetchData()
+    todo: Todo = Todo(id=len(todos),  title=str(title), description=str(description), completed=False)
+    todos.append(todo)
+    with open('todos.json', 'w') as file:
+        json.dump(todos, file)
+    return RedirectResponse('/')
+
+if __name__ == '__main__':
+    uvicorn.run(app)
